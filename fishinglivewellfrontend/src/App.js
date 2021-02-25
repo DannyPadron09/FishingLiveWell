b2c6b69a7a506a6baead9a61b781a45b08c866d8
DannyPadron09 <danny.padron09@gmail.com>
2021-02-25 17:26:58 -0500

    added in react for frontend

---
+++ fishinglivewellfrontend/src/App.js
@@ -0,0 +1,127 @@
+
+function App() {
+  return (
+    <div class="grid-container">
+            <header class="row">
+                <div>
+                    <a class="brand" href="index.html">Fishing Live Well</a>
+                </div>
+                <div>
+                    <a href="myLures.html">My Lures</a>
+                    <a href="signin.html">Sign In</a>
+                </div>
+            </header>
+            <main>
+                <div class="row center">
+                    <div class="card">
+                        <a href="lure.html">
+                            <img class="medium" src="./images/lure1.jpg" alt="lure" />
+                        </a>
+                        <div class="card-body">
+                            <a href="lure.html">
+                                <h2>BioSpawn VileCraw 4"</h2>
+                            </a>
+                            <div class="do-i-own">
+                                <span><i class="fas fa-check-square"></i></span>
+                                <span><i class="fas fa-check-square"></i></span>
+                            </div>
+                            <div class="price">
+                                $5
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <a href="lure.html">
+                            <img class="medium" src="./images/lure1.jpg" alt="lure" />
+                        </a>
+                        <div class="card-body">
+                            <a href="lure.html">
+                                <h2>BioSpawn VileCraw 4"</h2>
+                            </a>
+                            <div class="do-i-own">
+                                <span><i class="fas fa-check-square"></i></span>
+                                <span><i class="fas fa-check-square"></i></span>
+                            </div>
+                            <div class="price">
+                                $5
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <a href="lure.html">
+                            <img class="medium" src="./images/lure1.jpg" alt="lure" />
+                        </a>
+                        <div class="card-body">
+                            <a href="lure.html">
+                                <h2>BioSpawn VileCraw 4"</h2>
+                            </a>
+                            <div class="do-i-own">
+                                <span><i class="fas fa-check-square"></i></span>
+                                <span><i class="fas fa-check-square"></i></span>
+                            </div>
+                            <div class="price">
+                                $5
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <a href="lure.html">
+                            <img class="medium" src="./images/lure1.jpg" alt="lure" />
+                        </a>
+                        <div class="card-body">
+                            <a href="lure.html">
+                                <h2>BioSpawn VileCraw 4"</h2>
+                            </a>
+                            <div class="do-i-own">
+                                <span><i class="fas fa-check-square"></i></span>
+                                <span><i class="fas fa-check-square"></i></span>
+                            </div>
+                            <div class="price">
+                                $5
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <a href="lure.html">
+                            <img class="medium" src="./images/lure1.jpg" alt="lure" />
+                        </a>
+                        <div class="card-body">
+                            <a href="lure.html">
+                                <h2>BioSpawn VileCraw 4"</h2>
+                            </a>
+                            <div class="do-i-own">
+                                <span><i class="fas fa-check-square"></i></span>
+                                <span><i class="fas fa-check-square"></i></span>
+                            </div>
+                            <div class="price">
+                                $5
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <a href="lure.html">
+                            <img class="medium" src="./images/lure1.jpg" alt="lure" />
+                        </a>
+                        <div class="card-body">
+                            <a href="lure.html">
+                                <h2>BioSpawn VileCraw 4"</h2>
+                            </a>
+                            <div class="do-i-own">
+                                <span><i class="fas fa-check-square"></i></span>
+                                <span><i class="fas fa-check-square"></i></span>
+                            </div>
+                            <div class="price">
+                                $5
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <footer class="row center">
+                by: Danny Padron
+            </footer>
+        </div>
+  );
+}
+
+export default App;
