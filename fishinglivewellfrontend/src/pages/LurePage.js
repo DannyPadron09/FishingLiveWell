4058b5779c67ce9654d4b2bc6e4a72d0c44021c7
DannyPadron09 <danny.padron09@gmail.com>
2021-03-01 12:00:38 -0500

    lure links directing correctly to individual lure pages

--- fishinglivewellfrontend/src/pages/LurePage.js
+++ fishinglivewellfrontend/src/pages/LurePage.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import Lure from '../components/Lure'
+import data from '../data'
 
-export default function LurePage() {
+export default function LurePage(props) {
+
+    const lure = data.lures.find(x => x._id === props.match.params.id)
+
     return (
         <div>
-            Lure Page
+            <div className="row">
+                <div className="lure-picture">
+                    <img src={lure.lureImage} alt={lure.lureBrand} />
+                </div>
+                <div className="lure-stats">
+
+                </div>
+                <div className="add-lure">
+
+                </div>
+            </div>
         </div>
     )
 }
