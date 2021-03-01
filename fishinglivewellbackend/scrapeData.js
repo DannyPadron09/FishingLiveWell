6ad218de9fd77db499a6a03d2ab27332964bd456
DannyPadron09 <danny.padron09@gmail.com>
2021-03-01 16:32:32 -0500

    back to working order

--- fishinglivewellbackend/scrapeData.js
+++
@@ -1,5 +0,0 @@
-const scrapeData = () => {
-    fetch('https://shopkarls.com/tackle')
-        .then(res => res.json())
-        .then(json => console.log(json))
-}
--- fishinglivewellbackend/server.js
+++ fishinglivewellbackend/server.js
@@ -1,16 +1,17 @@
-import express from 'express'
-import data from './data.js'
+import express from 'express';
+import data from './data.js';
 
-const app = express()
+const app = express();
 
 app.get('/api/lures', (req, res) => {
-    res.send(data.lures)
-})
+  res.send(data.lures);
+});
 
 app.get('/', (req, res) => {
-    res.send('Server is ready')
-})
+  res.send('Server is ready');
+});
 
-app.listen(5000, () => {
-    console.log('Serve at http://localhost:5000')
-})
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
