f2f65dbaa26daba0858719d2ec82c6f61d35ab0b
DannyPadron09 <danny.padron09@gmail.com>
2021-03-01 13:23:43 -0500

    fetching data for pages from backend api

--- fishinglivewellfrontend/src/pages/HomePage.js
+++ fishinglivewellfrontend/src/pages/HomePage.js
@@ -1,13 +1,24 @@
-import React from 'react'
-import data from '../data'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import Lure from '../components/Lure'
 
 export default function HomePage() {
+
+    const [lures, setLures] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const { data } = await axios.get('/api/lures')
+            setLures(data)
+        }
+        fetchData()
+    }, [])
+
     return (
         <div>
             <div className="row center">
                 {
-                    data.lures.map(lure => (
+                    lures.map(lure => (
                         <Lure key={lure._id} lure={lure} />
                     ))
                 }
