20c05433b4424e53e3d02fb4360b0097f86b224d
DannyPadron09 <danny.padron09@gmail.com>
2021-03-09 15:15:23 -0500

    added individual lure reducer

--- fishinglivewellfrontend/src/reducers/lureReducer.js
+++ fishinglivewellfrontend/src/reducers/lureReducer.js
@@ -1,4 +1,4 @@
-import { LURE_LIST_FAIL, LURE_LIST_REQUEST, LURE_LIST_SUCCESS } from "../constant/lureConstant"
+import { LURE_DETAILS_FAIL, LURE_DETAILS_REQUEST, LURE_DETAILS_SUCCESS, LURE_LIST_FAIL, LURE_LIST_REQUEST, LURE_LIST_SUCCESS } from "../constant/lureConstant"
 
 export const lureListReducer = (state = {loading: true, lures:[]}, action) => {
 
@@ -17,3 +17,19 @@ export const lureListReducer = (state = {loading: true, lures:[]}, action) => {
             return state 
     }
 } 
+
+export const lureDetailsReducer = (state = { lure: {}, loading: true}, action) => {
+    switch (action.type) {
+        case LURE_DETAILS_REQUEST:
+            return { loading: true}
+
+        case LURE_DETAILS_SUCCESS:
+            return { loading: false, lure: action.payload}
+
+        case LURE_DETAILS_FAIL: 
+            return { loading: false, error: action.payload}
+
+        default: 
+            return state 
+    }
+}
