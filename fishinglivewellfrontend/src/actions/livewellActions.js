49b3c591c0163034162d49624572bb8aaece42a8
DannyPadron09 <danny.padron09@gmail.com>
2021-03-16 14:34:27 -0400

    fish being saved and displayed

--- fishinglivewellfrontend/src/actions/livewellActions.js
+++ fishinglivewellfrontend/src/actions/livewellActions.js
@@ -1,7 +1,7 @@
 import Axios from "axios"
 import { LIVEWELL_ADD_FISH } from "../constant/livewellConstant"
 
-export const addToLivewell = (lureId, fishCaught) => async(dispatch, getState) => {
+export const addToLivewell = (lureId, fishCaught) => async (dispatch, getState) => {
     const {data} = await Axios.get(`/api/lures/${lureId}`)
     dispatch({
         type: LIVEWELL_ADD_FISH,
@@ -12,5 +12,5 @@ export const addToLivewell = (lureId, fishCaught) => async(dispatch, getState) =
             fishCaught,
         }
     })
-    localStorage.setItem('livewellFish', JSON.stringify(getState().fishCaught))
+    localStorage.setItem('livewellFish', JSON.stringify(getState().livewell))
 }
